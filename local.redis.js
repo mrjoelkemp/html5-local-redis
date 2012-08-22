5d3a0b91ada01df09421003daf7ebbcb5709aaf0
Joel Kemp <mrjoelkemp@gmail.com>
2012-08-22 01:51:57 -0400

    Stubs

--- local.redis.js
+++ local.redis.js
@@ -23,7 +23,6 @@
   // Stores the passed value indexed by the passed key
   // Note: Auto stringifies
   proto.set = function(key, value) {
-
     // Stringify the key and value, if necessary
     value = (typeof value === "string") ? value : JSON.stringify(value);
     key   = (typeof key   === "string") ? key   : JSON.stringify(key);
@@ -36,12 +35,12 @@
   // Returns: A list of values for the passed key(s).
   // Note:    Values match keys by index.
   // Usage:   mget('key1', 'key2', 'key3') or mget(['key1', 'key2', 'key3'])
-  proto.mget = function(key) {
+  proto.mget = function(keys) {
     var results = [];
 
-    if (key instanceof Array) {
-      for (var i in key) {
-        results[results.length] = proto.get(key[i]);
+    if (keys instanceof Array) {
+      for (var i in keys) {
+        results[results.length] = proto.get(keys[i]);
       }
     } else {
       for (var j in arguments) {
@@ -51,5 +50,47 @@
 
     return results;
   };
+
+  // mset
+  // Allows the setting of multiple key value pairs
+  // Usage:   mset('key1', 'val1', 'key2', 'val2') or mset(['key1', 'val1', 'key2', 'val2'])
+  proto.mset = function (keyVals) {
+
+  // TODO: Allow passing in an object of keys values. Could this be something cool? {key1: val1, key2: val2}
+
+    if (keyVals instanceof Array) {
+      // If there's an odd number of elements, unset values default to undefined
+      for (var i = 0; i < keyVals.length; i += 2) {
+        proto.set(keyVals[i], keyVals[i + 1]);
+      }
+    } else {
+      for (var j = 0; j < arguments.length; j += 2) {
+        proto.set(arguments[j], arguments[j + 1]);
+      }
+    }
+  };
+
+  // incr
+  proto.incr = function (key) {
+  };
+
+  // incrby
+  proto.incrby = function (key, amount) {
+  };
+
+  // mincr
+  proto.mincr = function (keys) {
+  };
+
+  // mincrby
+  // Usage:   mincrby('key1', 1, 'key2', 4) or mincrby(['key1', 1, 'key2', 2])
+  proto.mincrby = function (keysAmounts) {
+  };
+
+  // decr
+
+  // decrby
+
+  // mdecrby
 
 })(window);
