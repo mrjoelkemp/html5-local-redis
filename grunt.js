e73fd97eecc98a9a9fb5e9340ad5b2583839c736
Joel Kemp <mrjoelkemp@gmail.com>
2012-09-14 15:16:47 -0400

    Grunt with minify

--- grunt.js
+++ grunt.js
@@ -3,55 +3,18 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: '<json:package.json>',
-    meta: {
-      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
-        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
-    },
-    lint: {
-      files: ['grunt.js', 'lib/**/*.js', 'spec/**/*.js']
-    },
     test: {
       files: ['spec/**/*.js']
     },
-    concat: {
-      dist: {
-        src: ['<banner:meta.banner>', '<file_strip_banner:lib/<%= pkg.name %>.js>'],
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
     min: {
       dist: {
-        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
-        dest: 'dist/<%= pkg.name %>.min.js'
+        src: ['src/local.redis.js'],
+        dest: 'src/local.redis.min.js'
       }
-    },
-    watch: {
-      files: '<config:lint.files>',
-      tasks: 'lint test'
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true
-      },
-      globals: {}
     },
     uglify: {}
   });
 
   // Default task.
-  grunt.registerTask('default', 'lint test concat min');
-
+  grunt.registerTask('default', 'test min');
 };
