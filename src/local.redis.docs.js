88bff12514e43e2b79432a4757afcb07f9ecf991
Joel Kemp <mrjoelkemp@gmail.com>
2012-09-24 16:36:26 -0400

    Keys api documented

--- src/local.redis.docs.js
+++ src/local.redis.docs.js
@@ -45,9 +45,9 @@ localRedis.set(null, null);
 // Removes the specified key(s).
 // *Returns* the number of keys removed.
 
-localRedis.del('foo');
-localRedis.del('foo', 'bar');
-localRedis.del(['foo', 'bar']);
+localRedis.set('foo', 'bar');
+localRedis.set('bar', 'foobar');
+localRedis.del('foo', 'bar');   // Returns 2
 
 // *Side effects:*
 // Clears existing expirations on keys, if set.
@@ -61,9 +61,10 @@ localRedis.del(['foo', 'bar']);
 // Determines whether or not the passed key exists in storage.
 // *Returns* `1` if the key exists, `0` otherwise.
 
-localRedis.exists({foo: 'bar'});
-localRedis.exists('foo');
-localRedis.exists(1234);
+localRedis.set({foo: 'bar'}, 'bar');
+localRedis.exists({foo: 'bar'});    // Returns 1
+localRedis.exists('foo');           // Returns 0
+localRedis.exists(1234);            // Returns 0
 
 
 
@@ -74,10 +75,16 @@ localRedis.exists(1234);
 // Renames `key` to `newkey`, transferring the value associated
 // with `key` to `newkey`.
 
-localRedis.rename('foo', 'bar');
 
 // *Side effects:* Transfers the `key`'s time-to-live (TTL)
 // (i.e., time until expiration) to `newkey`.
+
+localRedis.set('foo', 'foobar');
+localRedis.expire('foo', 2);
+localRedis.rename('foo', 'bar');
+localRedis.get('bar');            // Returns 'foobar'
+localRedis.get('foo');            // Returns null
+localRedis.expires('bar');        // Returns 1
 
 // *Throws* if `key == newkey` or `key` does not exist.
 
@@ -89,9 +96,11 @@ localRedis.rename('foo', 'bar');
 // Renames `key` to `newkey` *iff* `newkey` does not exist.
 // *Returns* `1` if `key` was renamed, `0` otherwise.
 
-localRedis.renamenx('foo', 'foobar');
+localRedis.set('foo', 'bar');
+localRedis.renamenx('foo', 'foobar'); // Returns 1
+localRedis.get('foobar');             // Returns 'bar'
 
-// *Side effects:* Same as `rename`.
+// *Side effects:* Same as `rename` in regards to key expiry.
 
 // *Throws* under the same conditions as `rename`.
 
@@ -106,8 +115,10 @@ localRedis.renamenx('foo', 'foobar');
 // *Returns* a single key or a list of keys if second parameter
 // is set to `true`.
 
-/* Assume 'foo' => 'bar' */
-localRedis.getkey('bar'); // Returns 'foo'
+localRedis.set('foo', 'bar');
+localRedis.set('bar', 'bar');
+localRedis.getkey('bar');       // Returns 'foo'
+localRedis.getkey('bar', true); // Returns ['foo', 'bar']
 
 // Note: This is a *custom*, non-Redis function.
 
@@ -120,11 +131,12 @@ localRedis.getkey('bar'); // Returns 'foo'
 // *Returns* `1` if the expiration was set, `0` if the
 // `key` does not exist or the expiration couldn't be set.
 
-localRedis.expire('foo', 2);
-localRedis.expire('bar', 1.5);
-
+localRedis.set('foo', 'bar');
+localRedis.expire('foo', 2);    // Returns 1
+localRedis.expire('bar', 1.5);  // Returns 0
 
 // [Read more](http://mrjoelkemp.com/2012/09/html5-local-storage-keyvalue-expiry/) about the expiration algorithm.
+
 
 
 
@@ -135,8 +147,10 @@ localRedis.expire('bar', 1.5);
 // Similar to `expire` except for the delay's units.
 // *Returns* the same as `expire`.
 
-localRedis.pexpire('foo', 500);
-localRedis.pexpire('bar', 10);
+localRedis.set('foo', 'bar');
+localRedis.set('bar', 'bar');
+localRedis.pexpire('foo', 500); // Returns 1
+localRedis.pexpire('bar', 10);  // Returns 1
 
 
 
@@ -149,7 +163,9 @@ localRedis.pexpire('bar', 10);
 // *Returns* `1` if there is expiration data for `key`,
 // `0` otherwise.
 
-localRedis.expires('foo'); // Returns 1
+localRedis.set('foo', 'bar');
+localRedis.expire('foo', 2);
+localRedis.expires('foo');   // Returns 1
 
 // Note: This is a *custom*, non-Redis function.
 
@@ -164,8 +180,10 @@ localRedis.expires('foo'); // Returns 1
 // *Returns* `1` if the expiration was set, `0` if the key
 // does not exist or the expiration couldn't be set.
 
-localRedis.expireat('foo', 1293840000);
+localRedis.set('foo', 'bar');
+localRedis.expireat('foo', 1348517858.724); // Returns 1
 
+// *Throws* if the timestamp has already passed.
 
 
 
@@ -177,8 +195,10 @@ localRedis.expireat('foo', 1293840000);
 // *Returns* the same as `expireat`.
 
 localRedis.set('foo', 'bar');
-localRedis.pexpireat('foo', 1348190381093280);  // Returns 1
-localRedis.pexpireat('foobar');                 // Returns 0
+localRedis.pexpireat('foo', 1348517858724); // Returns 1
+localRedis.pexpireat('foobar');             // Returns 0
+
+// *Throws* if the timestamp has already passed.
 
 
 
@@ -212,18 +232,48 @@ localRedis.ttl('foobar');     // Returns -1
 
 
 // ## pttl ##
-// *Usage:*
+// *Usage:* `pttl(key)`
+
+// Similar to `ttl` but returns the time-to-live of `key` in milliseconds,
+// or `-1` if the key does not exist or have an expiration.
+
+localRedis.set('foo', 'bar');
+localRedis.expire('foo', 2);
+localRedis.pttl('foo');        // Returns a few ms less than 2000
+localRedis.pttl('foobar');     // Returns -1
 
 
 
 // ## randomkey ##
-// *Usage:*
+// *Usage:* `randomkey()`
+
+// Returns a random key from storage or `null` when the datastore is empty.
+
+localRedis.randomkey();     // Returns null
+
+localRedis.set('foo', 'bar');
+localRedis.set('bar', 'foobar');
+localRedis.set('car', 'foo');
+
+localRedis.randomkey();     // Returns one of the keys
 
 
 
 
 // ## keys ##
-// *Usage:*
+// *Usage:* `keys(pattern)`
+
+// Finds all keys that match the (regular expression) pattern.
+// *Returns* a list of keys or a single key that matches `pattern`
+// or `null` if no keys were found to match `pattern`.
+
+localRedis.set('foo', 'bar');
+localRedis.set('bar', 'bar');
+localRedis.set('car', 'bar');
+
+localRedis.keys('ar');    // Returns ['bar', 'car']
+localRedis.keys('fo*');   // Returns 'foo'
+
 
 
 
